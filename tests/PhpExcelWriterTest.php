7f69f3a96388016b9e42eca9de32f75016d025d0
Satoru Hagiwara <hagiwara@fusic.co.jp>
2016-05-24 14:02:11 +0900

    初期コミット

---
+++ tests/PhpExcelWriterTest.php
@@ -0,0 +1,36 @@
+<?php
+namespace PhpExecl\Test;
+
+use PHPUnit_Framework_TestCase;
+
+
+class PhpExcelWriterTest extends PHPUnit_Framework_TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * tearDown method
+     *
+     * @return void
+     */
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+
+    /**
+     * Test initialize method
+     *
+     * @return void
+     */
+    public function test_make()
+    {
+
+    }
+
+
+}
